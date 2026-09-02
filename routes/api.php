12858a23fb7e68f02816d30961fba7ed61a333ef
Nesma <n.elsosey@gmail.com>
2026-09-02 10:34:12 +0300

    Fix api routes syntax

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,15 @@ use App\Http\Controllers\Api\user\SettingProfileController as UserSettingProfile
 use App\Http\Controllers\Api\coach\SettingProfileController as CoachSettingProfileController;
 use App\Http\Controllers\Api\Auth\EmailVerificationController;
 use App\Http\Controllers\Api\Auth\PasswordResetController;
-
+use App\Http\Controllers\Api\coach\AvailabilityController;
 use App\Http\Controllers\Api\coach\WorkoutPlanController;
 use App\Http\Controllers\Api\coach\NutritionPlanController;
 use App\Http\Controllers\Api\coach\ProgressController;
 use App\Http\Controllers\Api\user\UserProfileController;
 use App\Http\Controllers\Api\coach\SubscriptionController as CoachSubscriptionController;
-use App\Http\Controllers\Api\coach\WorkoutPlanController;
+
 use App\Http\Controllers\Api\General\BrowseCoachController;
+use App\Http\Controllers\Api\user\RequestSubscriptionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +24,7 @@ Route::post('/login/google', [AuthController::class, 'loginWithGoogle']);
 
 // Protected Routes (Sanctum)
 Route::middleware('auth:sanctum')->group(function () {
-    
+
     Route::post('/logout', [AuthController::class, 'logout']);
 
     // Trainee / User Profile Routes
@@ -32,46 +33,53 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/update', [UserSettingProfileController::class, 'update']);
         Route::put('/update/avatar', [UserSettingProfileController::class, 'updateProfilePhoto']);
     });
-    //
+    //public Rout
     Route::get('/coaches', [BrowseCoachController::class, 'index']);
+    Route::post('/RequestSubscription', [
+        RequestSubscriptionController::class,
+        'requestSubscription'
+    ]);
+    Route::get('/MyRequests', [
+        RequestSubscriptionController::class,
+        'getMyRequests'
+    ]);
 
     // Coach Routes
-    Route::prefix('coach')->group(function () {       
+    Route::prefix('coach')->group(function () {
         // Coach Profile
         Route::prefix('profile/setting')->group(function () {
             Route::get('/show', [CoachSettingProfileController::class, 'show']);
             Route::post('/update', [CoachSettingProfileController::class, 'update']);
-             Route::put('/update/avatar', [CoachSettingProfileController::class, 'updateProfilePhoto']);
+            Route::put('/update/avatar', [CoachSettingProfileController::class, 'updateProfilePhoto']);
         });
         // Coach Packages
-        Route::apiResource('packages', PackageController::class);
+        // Route::apiResource('packages', PackageController::class);
         // Coach Availabilities (Working Days & Slots)
         Route::get('/availabilities', [AvailabilityController::class, 'index']);
         Route::post('/availabilities', [AvailabilityController::class, 'store']);
-
     });
 
 
-        // Trainees & Plans Routes
-        Route::get('/trainees/{id}', [CoachSettingProfileController::class, 'showTraineeDetails']);
-        Route::post('/trainees/{id}/workout-plan', [WorkoutPlanController::class, 'storeOrUpdate']);
-        Route::post('/trainees/{id}/nutrition-plan', [NutritionPlanController::class, 'storeOrUpdate']);
-        Route::get('/trainees/{id}/progress', [ProgressController::class, 'show']);
-    });
-
-    Route::get('/user', function (Request $request) {
-        return response()->json([
-            'status' => true,
-            'user' => $request->user()
-        ]);
-    });
+    // Trainees & Plans Routes
+    Route::get('/trainees/{id}', [CoachSettingProfileController::class, 'showTraineeDetails']);
+    Route::post('/trainees/{id}/workout-plan', [WorkoutPlanController::class, 'storeOrUpdate']);
+    Route::post('/trainees/{id}/nutrition-plan', [NutritionPlanController::class, 'storeOrUpdate']);
+    Route::get('/trainees/{id}/progress', [ProgressController::class, 'show']);
     // Subscription & Trainees Management
-        Route::get('/subscriptions/pending', [CoachSubscriptionController::class, 'pendingRequests']);
-        Route::put('/subscriptions/{id}/accept', [CoachSubscriptionController::class, 'acceptRequest']);
-        Route::put('/subscriptions/{id}/reject', [CoachSubscriptionController::class, 'rejectRequest']);
-        Route::get('/trainees', [CoachSubscriptionController::class, 'myTrainees']);
-        Route::get('/trainees/{trainee_id}', [CoachSubscriptionController::class, 'showTraineeDetails']);
+    Route::get('/subscriptions/pending', [CoachSubscriptionController::class, 'pendingRequests']);
+    Route::put('/subscriptions/{id}/accept', [CoachSubscriptionController::class, 'acceptRequest']);
+    Route::put('/subscriptions/{id}/reject', [CoachSubscriptionController::class, 'rejectRequest']);
+    Route::get('/trainees', [CoachSubscriptionController::class, 'myTrainees']);
+    Route::get('/trainees/{trainee_id}', [CoachSubscriptionController::class, 'showTraineeDetails']);
 });
+
+Route::get('/user', function (Request $request) {
+    return response()->json([
+        'status' => true,
+        'user' => $request->user()
+    ]);
+});
+
 
 // Public User Profile Routes
 Route::prefix('user')->group(function () {
